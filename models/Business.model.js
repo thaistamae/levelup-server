56ec794e785ee66864628795aedce5bc4c89ba7d
Joe D <joedeejr@gmail.com>
2022-03-02 17:41:14 -0300

    minimal changes

--- models/Business.model.js
+++ models/Business.model.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const BusinessSchema = new Schema({
   name: { type: String, required: true, trim: true },
-  dba: { type: String, required: true, trim: true },
+  dba: { type: String, trim: true },
   email: {
     type: String,
     required: true,
@@ -11,16 +11,19 @@ const BusinessSchema = new Schema({
     lowercase: true,
   },
   passwordHash: { type: String, required: true },
-  CNPJ: { type: Number, maxLength: 14, required: true, trim: true  },
-  phone: { type: Number, required: true, trim: true},
+  CNPJ: { type: Number, maxLength: 14, trim: true  },
+  birthDate: { type: Date },
+  phone: { type: Number, required: true, trim: true },
   role: {
     type: String,
-    enum: ["ADMIN", "BUSINESS"], 
+    enum: ["BUSINESS", "CUSTOMER"], 
     required: true,
     default: "BUSINESS",
   },
+  address: { type: String, required: true, trim: true },
   image: { type: String, default: "/" }
 });
+
 
 const BusinessModel = model("Business", BusinessSchema);
 
--- models/Points.model.js
+++ models/Points.model.js
@@ -1,7 +1,8 @@
 const { Schema, model } = require("mongoose");
 
 const PointsSchema = new Schema({
-  //
+    //
+  
 
 });
 
