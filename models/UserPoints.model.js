3994cbe02c1cddba5a279d708e45e55ac87d65c3
Thais Tamae <thais.tamae@gmail.com>
2022-03-05 18:12:08 -0300

    UserPoint fixed with points accumulated rule

--- models/UserPoints.model.js
+++ models/UserPoints.model.js
@@ -3,11 +3,8 @@ const mongoose = require("mongoose");
 
 const UserPointsSchema = new Schema({
   businessId: { type: mongoose.Schema.Types.ObjectId, ref: "Business" },
-  pointId: { type: mongoose.Schema.Types.ObjectId, ref: "Points" },
   customerEmail: { type: String, required: true },
   pointsInThisPromotion: { type: Number, required: true },
-  deadline: { type: Date, min: Date.now, default: null },
-  goal: { type: String, required: true },
 });
 
 const UserPointsModel = model("UserPoints", UserPointsSchema);
