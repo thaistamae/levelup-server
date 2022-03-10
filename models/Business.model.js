0eb2459f8f565b192ce41f891e0fc0f7cb4ca895
Larissa Ferreira <larissadantasaf@gmail.com>
2022-03-10 10:32:59 +0100

    Fix business model to validate

--- models/Business.model.js
+++ models/Business.model.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const BusinessSchema = new Schema({
   name: { type: String, required: true, trim: true },
-  dba: { type: String, trim: true },
+  dba: { type: String, required: true, trim: true },
   email: {
     type: String,
     required: true,
@@ -12,28 +12,18 @@ const BusinessSchema = new Schema({
   },
   passwordHash: { type: String, required: true },
   CNPJ: { type: String, maxLength: 14, trim: true },
-  CPF: { type: String, maxLength: 11, trim: true },
-  birthDate: { type: Date },
   phone: { type: Number, required: true, trim: true },
   role: {
     type: String,
     enum: ["BUSINESS", "CUSTOMER"],
-    /*required: true,*/
     default: "BUSINESS",
   },
-  address: new Schema({
-    street: { type: String, required: true },
-    number: { type: Number, required: true },
-    neighborhood: { type: String, required: true },
-    city: { type: String, required: true },
-    state: {
-      type: String,
-      required: true,
-      uppercase: true,
-    },
-    zipcode: { type: String, maxLength: 8, required: true },
-  }),
-  profilePicture: { type: String, default: null },
+  address: { type: String, required: true },
+  profileAvatarUrl: { 
+    type: String,
+    trim: true, 
+    default: "/",
+  },
   isDeleted: { type: Boolean, default: false },
   deletedDate: { type: Date },
   resetPassword: { type: String, default: "" },
