e0fae29641611149c5539531899dcc53a547e6b3
Larissa Ferreira <larissadantasaf@gmail.com>
2022-03-11 01:21:33 +0100

    Add comments

--- routes/uploadImages.routes.js
+++ routes/uploadImages.routes.js
@@ -1,14 +1,14 @@
-const router = require("express").Router();
+// const router = require("express").Router();
 
-const uploadCloud = require("../config/cloudinary");
+// const uploadCloud = require("../config/cloudinary");
 
-router.post("/", uploadCloud.single("picture"), (req, res) => {
-  if (!req.file) {
-    return res.status(500).json({ message: "Upload falhou" });
-  }
+// router.post("/upload", uploadCloud.single("picture"), (req, res) => {
+//   if (!req.file) {
+//     return res.status(500).json({ message: "Upload falhou" });
+//   }
 
-  return res.status(201).json({ url: req.file.path });
-});
+//   return res.status(201).json({ url: req.file.path });
+// });
 
-module.exports = router;
+// module.exports = router;
 
